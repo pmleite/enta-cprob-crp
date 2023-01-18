4b438cb7a08e1b98eff998c026c4d860e808d9c9
pmrleite <paulo.miguel.r.leite@gmail.com>
2023-01-18 16:55:56 -0100

    Add some documents and images, update do sketch do segue linha

--- segue_linha/segue_linha.ino
+++ segue_linha/segue_linha.ino
@@ -5,7 +5,17 @@ void setup() {
 
   Serial.begin(BAUD_RATE);
 
-  pinMode()
+  //Pinos dos IR
+  pinMode(IR_LEFT_PIN, INPUT);
+  pinMode(IR_CENTER_PIN, INPUT);
+  pinMode(IR_RIGHT_PIN, INPUT);
+
+  //Pinos de controlo do motor
+  pinMode(MOTOR_1_PIN_A, OUTPUT);
+  pinMode(MOTOR_1_PIN_B, OUTPUT);
+  pinMode(MOTOR_2_PIN_A, OUTPUT);
+  pinMode(MOTOR_2_PIN_B, OUTPUT);
+
 
 }
 
--- segue_linha/settings.h
+++ segue_linha/settings.h
@@ -7,6 +7,25 @@
 #define MOTOR_2_PIN_A  10
 #define MOTOR_2_PIN_B  11
 
+#define MOTOR_1_EN_PIN 12  //Deve ser PWM
+#define MOTOR_2_EN_PIN 13  //Deve ser PWM
+
 #define BAUD_RATE      9600
 
+/*
+  S1	  S2	  S3	  S4	  ---> Result
+  1	    0	    0	    1	    ---> Motor turn right
+  0	    1	    1	    0   	---> Motor turn left
 
+  0	    0	    0	    0     |
+  1	    0	    0	    0     |
+  0	    1	    0   	0     |--> Motor Coasts
+  0	    0	    1	    0     |
+  0	    0   	0   	1     |
+
+  0	    1	    0	    1	    |--> Motor Brake
+  1	    0	    1	    0     |--> Motor Brake
+
+  X	    X	    1	    1	    |--> Short circuit
+  1	    1   	X	    X     |--> Short circuit
+*/
