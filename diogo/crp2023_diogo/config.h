fa626af17dd2c390da47cf30fe6f63c182d04f8e
Swift132 <diogopacheco132@gmail.com>
2023-01-21 18:52:43 -0100

    Combined Codes

--- diogo/crp2023_diogo/config.h
+++ diogo/crp2023_diogo/config.h
@@ -1,12 +1,13 @@
+#include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h> 
 
 /************************************************
      Esta secção rerefe-se apenas às
      configurações genéricas
  ************************************************/
-#define COLOR_DET_CALIBRATION true  //FALSE Modo de leitura TRUE Modo de calibração
-#define BAUT_RATE             9600
+#define COLOR_DET_CALIBRATION false  //FALSE Modo de leitura TRUE Modo de calibração
+#define PULSE_FREQ            50     //Frequencia base de trabalho do MUX PWM
+#define BAUD_RATE             9600
 #define COLOR_READ_DELAY      100
 
 /************************************************
@@ -88,3 +89,48 @@ int servGarraLastPos =   0;
 int servBaseLastPos  =   0;
 int servVertLastPos  =   0;
 int servHoriLastPos  =   0;
+
+
+/************************************************
+     Esta seclção refere-se apenas às 
+     configurações necessárias para os
+     SENSORES Infra-Vermelhos
+ ************************************************/
+#define IR_LEFT_PIN    2
+#define IR_CENTER_PIN  4
+#define IR_RIGHT_PIN   7
+
+#define SLOW_SPEED      20
+#define MED_SPEED       15
+#define FAST_SPEED      8
+
+/************************************************
+     Esta seclção refere-se apenas às 
+     configurações necessárias para os MOTORES 
+ ************************************************/
+
+//Tensão de trabalho do motor e Vin. Necessário para calcular o PULSO Máximo
+#define MOTOR_VOLTAGE    5
+#define POWER_VOLTAGE_IN 6
+
+/* 
+ * Estes dois valores são depois convertidos com a função convertSpeedToPulse() 
+ * numa escala de 0 a 100, sendo que 0 é a velocidade minima possivel de andamento
+ * obtida com uma calibração de velocidade minima.
+ */
+#define MOTOR_MIN_PULSE  1000 //Pulso para a velocidade mínima (Obter com calibração)
+#define MOTOR_MAX_PULSE  4096 * MOTOR_VOLTAGE / POWER_VOLTAGE_IN  //VMAX -  Calculado com a fórmula (4096 * Vm/Vin)
+
+/*
+ * Constantes que irão definir a diferenca de velocidade entre
+ * motores para fazer uma curva suave ou mais acentuada.
+ * Na prática será o resultado de MOTOR_MAX_PULSE - (um valaor a definir)
+ */
+#define DIF_TURN_SPD_S   MOTOR_MAX_PULSE - 300
+#define DIF_TURN_SPD_F   MOTOR_MAX_PULSE - 1000
+
+//Motor PWM - Index
+#define MOTOR_1_A        8   //PWM MUX - Pino S1 do motor 1
+#define MOTOR_1_B        9   //PWM MUX - Pino S2 do motor 1
+#define MOTOR_2_A        10  //PWM MUX - Pino S1 do motor 2
+#define MOTOR_2_B        11  //PWM MUX - Pino S2 do motor 2
--- diogo/crp2023_diogo/crp2023_diogo.ino
+++ diogo/crp2023_diogo/crp2023_diogo.ino
@@ -1,10 +1,21 @@
 #include "config.h"
 #include "utils.h"
+#include "motor_moves.h"
 
 void setup() {
+  Serial.begin(BAUD_RATE);
 
-  Serial.begin(BAUT_RATE);
+  //Pinos dos IR
+  pinMode(IR_LEFT_PIN, INPUT);
+  pinMode(IR_CENTER_PIN, INPUT);
+  pinMode(IR_RIGHT_PIN, INPUT);
+  
+  servoMUX.begin();
+  servoMUX.setPWMFreq(PULSE_FREQ);
   delay(500);
+
+  stop();
+  delay(200);
 
   //Definição das GPIO do detetor de cor
   pinMode(S0, OUTPUT);
@@ -16,12 +27,12 @@ void setup() {
   //Ajusta a amplitude do pulse para 20% (Varia se for arduino, raspbery, etc)
 	digitalWrite(S0,HIGH);
 	digitalWrite(S1,LOW);
+
+  armInitPosition();
 }
 
 void loop() {
-
   
-
   updateColorValues();
   printCalibrationValues();
   //wichColor(redValue, greenValue, blueValue, whiteValue);
